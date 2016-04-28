ad7ad905bbb16c3820642770bd8cc9956d2db6b0
yuyuxuan <yuyuxuan@outlook.com>
2016-04-28 15:29:58 -0500

    04-28-2016

--- get_GITHUB.py
+++ get_GITHUB.py
@@ -15,7 +15,14 @@ if __name__ == '__main__':
     silentremove(filename)
     token=input('Enter token here: ') # "149***************c8d"
     username=input('Enter username here: ') #yu-yuxuan
-    url="https://api.github.com/users/%s/repos?access_token=%s"  % (username,token)
+    type=input('Enter type (repos, starred, subscriptions) here: ') #
+    if token=="":
+        url="https://api.github.com/users/%s/%s?per_page=100"  % (username,type)
+    else:
+        url="https://api.github.com/users/%s/%s?access_token=%s&per_page=100"  % (username,type,token)
+
+    # https://api.github.com/users/yu-yuxuan/starred?per_page=100
+    # https://api.github.com/users/yu-yuxuan/subscriptions?per_page=100
     allProjects     = urlopen(url)
     allProjectsDict = json.loads(allProjects.read().decode())
     for thisProject in allProjectsDict:
